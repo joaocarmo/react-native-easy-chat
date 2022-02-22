3c86a7b0beb13640174ac449e9a801acce447faf
João Carmo <kingjoao@gmail.com>
2022-02-22 00:52:09 +0000

    Change reference in MessageVideo

--- src/MessageVideo.tsx
+++ src/MessageVideo.tsx
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native'
 export default (_props: any) => (
   <View style={{ padding: 20 }}>
     <Text style={{ color: Color.alizarin, fontWeight: '600' }}>
-      Video is not implemented by GiftedChat.
+      Video is not implemented by EasyChat.
     </Text>
     <Text style={{ color: Color.alizarin, fontWeight: '600' }}>
       You need to provide your own implementation by using renderMessageVideo
